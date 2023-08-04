8ec43c58fb66e49c217672ce6c3f20ea1027495c
Sabine <srchu@mit.edu>
2023-08-04 12:42:51 -0400

    added documentation

--- src/MIT-Neutron-Diffractometer/3he_detector_reader.py
+++ src/MIT-Neutron-Diffractometer/3he_detector_reader.py
@@ -3,7 +3,7 @@
 Sabine Chu
 08-01-2023
 
-Implements basic packet reader.
+Implements basic packet reader
 """
 
 import socket
@@ -20,22 +20,22 @@ INST_TIME = b"\x6c"
 
 class detector_reader:
     def __init__(self, ip_address=IP_ADDRESS, port=PORT):
+        """
+        Creates new socket object linked to detectors
+        Inputs: ip_address (string), port (int)
+        Default inputs given by Canon documentation for TCP connection
+        """
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
         self.ip = ip_address
         self.port = port
 
-    # def collect_8bytes(self):
-    #     self.hex_data = ""
-    #     for j in range(8):
-    #         self.hex_data += self.sock.recv(1).hex()
-
     def collect_8bytes(self):
+        # Collect bytes 1-by-1 due to socket's unpredictability re length of data
         self.bytes_data = bytearray(b"")
         for j in range(8):
             self.bytes_data += self.sock.recv(1)
 
     def translate_5f(self):
-        # psd_number, position = translate_neutron_data(self.hex_data)
         psd_number, position = translate_neutron_data(self.bytes_data)
         if position:
             res = BINS-1 if position*BINS >= BINS else int(position*BINS)
@@ -46,7 +46,12 @@ class detector_reader:
                 self.arr7[res] += 1
                 self.count7 += 1
 
-    def start(self, time_len, test_label, save=True):
+    def start(self, seconds, test_label, save=True, fldr=""):
+        """"
+        Connects to detector and reads data for given time
+        Can save data (in folder if given)
+        Inputs: seconds (float), test_label (string), save (bool), fldr (string)
+        """
         self.sock.connect((self.ip, self.port))
         self.arr0, self.arr7 = np.zeros(BINS), np.zeros(BINS)
         self.count0 = 0
@@ -55,43 +60,36 @@ class detector_reader:
         self.current_time = 0
         while not self.start_time:
             self.collect_8bytes()
-            # if self.hex_data[:2] == "5f":
             if self.bytes_data[1] == NEUTRON_EVENT:
                 self.translate_5f()
-            # elif self.hex_data[:2] == "6c":
-            #    self.start_time = translate_instrument_time(self.hex_data)
             elif self.bytes_data[1] == INST_TIME:
                 self.start_time = translate_instrument_time(self.bytes_data)
-        while self.current_time - self.start_time < time_len:
+        while self.current_time - self.start_time < seconds:
             self.collect_8bytes()
-            # if self.hex_data[:2] == "5f":
             if self.bytes_data[1] == NEUTRON_EVENT:
                 self.translate_5f()
-            # elif self.hex_data[:2] == "6c":
-            #     self.current_time = translate_instrument_time(self.hex_data)
             elif self.bytes_data[1] == INST_TIME:
                 self.current_time = translate_instrument_time(self.bytes_data)
         self.sock.close()
 
         if save:
-            np.savetxt(f"{test_label}_arr0.txt", self.arr0)
-            np.savetxt(f"{test_label}_arr7.txt", self.arr7)
+            np.savetxt(f"{fldr}/{test_label}_detector0_histogram.txt", self.arr0)
+            np.savetxt(f"{fldr}/{test_label}_detector7_histogram.txt", self.arr7)
             plt.plot(self.arr0, label="psd 0")
             plt.plot(self.arr7, label="psd 7")
             plt.legend()
             plt.xlabel("position")
             plt.ylabel("neutron count")
             plt.title(test_label)
-            plt.savefig(f"{test_label}_graph.png")
+            plt.savefig(f"{fldr}/{test_label}_graph.png")
 
     def sanity_check(self, pings=SANITY_PINGS):
+        """
+        Prints out given number of packets
+        """
         self.sock.connect((self.ip, self.port))
         for i in range(pings):
             self.collect_8bytes()
-            # if self.hex_data[:2] == "5f":
-            #     print(translate_neutron_data(self.hex_data))
-            # elif self.hex_data[:2] == "6c":
-            #     print(translate_instrument_time(self.hex_data))
             if self.bytes_data[1] == NEUTRON_EVENT:
                 print(translate_neutron_data(self.bytes_data))
             elif self.bytes_data[1] == INST_TIME:
--- src/MIT-Neutron-Diffractometer/translaters.py
+++ src/MIT-Neutron-Diffractometer/translaters.py
@@ -3,11 +3,12 @@ translaters.py
 Sabine Chu
 08-01-2023
 
-Translates data from detectors.
+Translates data from detectors
+Formulae from Canon documentation
 """
 
 def translate_neutron_data(bin_data):
-    time = 25*10**-9*(2**16*bin_data[1] + 2**8*bin_data[2] + bin_data[3]) # time in nanoseconds
+    nanoseconds = 25*10**-9*(2**16*bin_data[1] + 2**8*bin_data[2] + bin_data[3])
     psd_number = (bin_data[4] // 2**4) % 2**3
     pl = 2**10*(bin_data[4] % 2**4) + 2**2*bin_data[5] + bin_data[6] // 2**6
     pr = 2**8*(bin_data[6] % 2**6) + bin_data[7]
@@ -18,11 +19,12 @@ def translate_neutron_data(bin_data):
         position = None
     return psd_number, position
 
-def translate_trigger_id(bin_data): # not used
+# We don't use trigger data or TOF
+def translate_trigger_id(bin_data):
     return bin_data[1], bin_data[2], bin_data[3]
 
-def translate_instrument_time(bin_data): # time in seconds
-    ii = 2**24*bin_data[1] + 2**16*bin_data[2] + 2**8*bin_data[3] + bin_data[4]
-    s = 2**-15*(2**7*bin_data[5] + bin_data[6] // 2**1)
-    s1 = 25*10**-8*(2**8*(bin_data[6] % 2**3) + bin_data[7])
-    return ii + s + s1
+def translate_instrument_time(bin_data):
+    seconds1 = 2**24*bin_data[1] + 2**16*bin_data[2] + 2**8*bin_data[3] + bin_data[4]
+    seconds2 = 2**-15*(2**7*bin_data[5] + bin_data[6] // 2**1)
+    seconds3 = 25*10**-8*(2**8*(bin_data[6] % 2**3) + bin_data[7])
+    return seconds1 + seconds2 + seconds3
