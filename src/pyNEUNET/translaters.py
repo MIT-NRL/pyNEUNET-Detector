ef4360730d486c7839ed4185fea64ebd703754cb
Sabine <srchu@mit.edu>
2023-08-16 14:16:07 -0400

    format fix

--- src/pyNEUNET/translaters.py
+++ src/pyNEUNET/translaters.py
@@ -10,7 +10,7 @@ EFFECT_LEN_MM = 150
 ANODE_RES = 1.5 # kilo-ohms
 PREAMP_RES = 1
 
-def translate_neutron_data(bin_data, type=14):
+def translate_neutron_data(bin_data, resolution_type=14):
     """
     Translates 8-byte neutron data
 
@@ -29,24 +29,23 @@ def translate_neutron_data(bin_data, type=14):
             Value ranging from 0 to 1 corresponding to position on detector
             See to_physical_position function to convert to physical position
     """
-    if type == 12:
-        nanoseconds = 25*10**-9*(2**16*bin_data[1] + 2**8*bin_data[2] + bin_data[3])
+    # nanoseconds = 25*10**-9*(2**16*bin_data[1] + 2**8*bin_data[2] + bin_data[3])
+    if resolution_type == 12:
         psd_number = bin_data[4] % 2**3
-        pl =2**4*bin_data[5] + bin_data[6] // 2**4
-        pr = 2**8*(bin_data[6] % 2**4) + bin_data[7]
-    elif type == 14:
-        nanoseconds = 25*10**-9*(2**16*bin_data[1] + 2**8*bin_data[2] + bin_data[3])
+        pulse_left = 2**4*bin_data[5] + bin_data[6] // 2**4
+        pulse_right = 2**8*(bin_data[6] % 2**4) + bin_data[7]
+    elif resolution_type == 14:
         psd_number = (bin_data[4] // 2**4) % 2**3
-        pl = 2**10*(bin_data[4] % 2**4) + 2**2*bin_data[5] + bin_data[6] // 2**6
-        pr = 2**8*(bin_data[6] % 2**6) + bin_data[7]
-    pulse_height = pl + pr
+        pulse_left = 2**10*(bin_data[4] % 2**4) + 2**2*bin_data[5] + bin_data[6] // 2**6
+        pulse_right = 2**8*(bin_data[6] % 2**6) + bin_data[7]
+    pulse_height = pulse_left + pulse_right
     try:
-        position = pl/pulse_height
+        position = pulse_left/pulse_height
     except ZeroDivisionError:
         position = None
     return psd_number, position
 
-def translate_instrument_time(input=None, mode='seconds'):
+def translate_instrument_time(inp=None, mode='seconds'):
     '''
     Function to convert time from bytes to seconds and the reverse.
     Time is defined as seconds since 2008 and stored in 5 bytes.
@@ -61,23 +60,23 @@ def translate_instrument_time(input=None, mode='seconds'):
         'datetime' : output time in datetime
 
     '''
-    if input is None:
-        secondsSince2008 = (datetime.now() - datetime(2008,1,1,0,0)).total_seconds()
-        secondsBytes = int(secondsSince2008).to_bytes(4,'big')
-        subsecondsBytes = int(secondsSince2008 % 1 * 2**8).to_bytes(1,'big')
-        return secondsBytes + subsecondsBytes
-    elif isinstance(input,(bytes,bytearray)):
-        intSeconds = int.from_bytes(input[:4],'big')
-        intSubSeconds = input[4]
-        secondsSince2008 = intSeconds + (intSubSeconds / 2**8)
-        if mode == 'seconds':
-            return secondsSince2008
-        elif mode == 'datetime':
-            return datetime(2008,1,1) + timedelta(seconds=secondsSince2008)
-    elif isinstance(input,(int,float)):
+    if inp is None:
+        seconds_since_2008 = (datetime.now() - datetime(2008,1,1,0,0)).total_seconds()
+        seconds_bytes = int(seconds_since_2008).to_bytes(4,'big')
+        subseconds_bytes = int(seconds_since_2008 % 1 * 2**8).to_bytes(1,'big')
+        return seconds_bytes + subseconds_bytes
+    if isinstance(inp,(int,float)):
         return datetime(2008,1,1,0,0) + timedelta(seconds=input)
-    elif isinstance(input,datetime):
-        return (input - datetime(2008,1,1,0,0)).total_seconds()
+    if isinstance(inp,datetime):
+        return (inp - datetime(2008,1,1,0,0)).total_seconds()
+    if isinstance(inp,(bytes,bytearray)):
+        int_seconds = int.from_bytes(inp[:4],'big')
+        int_subseconds = inp[4]
+        seconds_since_2008 = int_seconds + (int_subseconds / 2**8)
+        if mode == 'seconds':
+            return seconds_since_2008
+        if mode == 'datetime':
+            return datetime(2008,1,1) + timedelta(seconds=seconds_since_2008)
 
 def to_physical_position(decimal_pos):
     """
@@ -95,27 +94,3 @@ def to_physical_position(decimal_pos):
     """
     physical_pos = (decimal_pos - 0.5)*EFFECT_LEN_MM*(ANODE_RES + 2*PREAMP_RES)/ANODE_RES
     return physical_pos
-
-# NOT USED
-def translate_trigger_id(bin_data):
-    return bin_data[1], bin_data[2], bin_data[3:]
-
-def from_bytes_instrument_time(bin_data):
-    """
-    Translates 8-byte instrument time data
-
-    Parameters
-    -------
-    bin_data: bytes
-            8-byte binary data from detector
-
-    Returns
-    -------
-    seconds: float
-            Number of seconds since 1/1/2008 0:00:00
-    """
-    seconds1 = 2**24*bin_data[1] + 2**16*bin_data[2] + 2**8*bin_data[3] + bin_data[4]
-    seconds2 = 2**-15*(2**7*bin_data[5] + bin_data[6] // 2**1)
-    seconds3 = 25*10**-8*(2**8*(bin_data[6] % 2**3) + bin_data[7])
-    seconds = seconds1 + seconds2 + seconds3
-    return seconds
